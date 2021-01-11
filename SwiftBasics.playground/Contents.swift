bbeb89ccef7fe2871caa756fbf247ad7e9b6378e
Галым Тлектесов <galymtlektesov@MacBook-Air-Galym.local>
2021-01-11 18:36:37 +0600

    Глава 5.15 страница 72

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -8,7 +8,7 @@ import UIKit
 Введите в аргуметы данные с типо Int, и функция вычислит наибольший среди них числовой параметр
  - parameter a:первое число
  - parameter b:второе число*/
-public func whoIsBigger(a: Int, b: Int){
+public func whoIsBigger(a: Int, b: UInt){
     if a > b{
         print("\(a) > \(b)")
     }
@@ -17,13 +17,75 @@ public func whoIsBigger(a: Int, b: Int){
     }
 }
 
-var a = 324
-var b = 43
+var drogonName: String
+var a1 = 34
+var b2 = 7
 
-var c = "Summ: " + String(a + b)
+var c3 = "Summ: " + String(a1 + b2)
+drogonName = c3
 
-
-let Group = 101, 🎉 = 710
+let Group = Int.max, 🎉 = UInt.max
 let `var` = 7
 whoIsBigger(a: Group, b: 🎉)
+print(a1 % b2)
+var intMax = Int.max
+var numOne: Int8 = 0
+// Ошибка слишком большое значение numOne = Int8(intMax)
+print(numOne)
+//: **Задние**
+//: *1*
+var x: UInt8
+var y: Int8
+x = UInt8.max
+y = Int8.min
+print(x)
+print(y)
+
+var varOne: Int
+var varTwo = 73
+varOne = 53
+
+var temp = varOne
+varOne = varTwo
+varTwo = temp
+
+print("varOne=\(varOne)")
+print("varTwo=\(varTwo) \n")
+
+//: *1*
+//: *2*
+let floatOne: Float = 34.23
+let floatTwo: Float
+var numDouble: Double
+//: *2*
+//: *3.1*
+let intNum: Int, floatNum: Float, doubleNUm: Double
+intNum = 18; floatNum = 16.4; doubleNUm = 5.7
+
+let summFloat = Float(intNum) + Float(doubleNUm) + floatNum
+let summInt = intNum + Int(doubleNUm) + Int(floatNum)
+
+let someNumDouble = Double(floatNum) / doubleNUm
+
+print("При вычислении суммы типа float: \(summFloat) = \(intNum) + \(Float(doubleNUm)) + \(floatNum)")
+print("При вычислении суммы типа int: \(summInt) = \(intNum) + \(Int(doubleNUm)) + \(Int(floatNum))")
+print("При вычислении остатка типа double: \(someNumDouble) = \(Double(floatNum)) % \(doubleNUm)")
+//: *3.2
+var a = 2, b = 3
+var dis = (a + 4 * b) * (a - 3 * b) + a*a
+print(dis)
+//: *3.3*
+var c = 1.75, d = 3.25
+print(2 * ((c + 1) * (c + 1)) + 3 * (d + 1))
+//: *3.4*
+let lenght: Float = 7
+
+let S = lenght * lenght
+print("S=\(S)")
+let l = 3.14 * (lenght * 2)
+print("l=\(l)")
+
+//: *3*
+
+
 
